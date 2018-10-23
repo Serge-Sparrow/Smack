ec1ce024178c9df3ff68e37ebda0d3bef9ad2716
Serhii Vorobiov <serhiivorobiov@Sparrow.local>
2018-10-22 23:19:38 -0700

    First test

--- app/src/androidTest/java/com/example/serhiivorobiov/smack/TestFramework/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/serhiivorobiov/smack/TestFramework/ExampleInstrumentedTest.kt
@@ -1,26 +1,35 @@
-package com.example.serhiivorobiov.smack
+package com.example.serhiivorobiov.smack.TestFramework
 
-import android.support.test.InstrumentationRegistry
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withContentDescription
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-
+import com.example.serhiivorobiov.smack.Controller.MainActivity
+import com.example.serhiivorobiov.smack.R
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.Rule
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class gitExampleInstrumentedTest {
+class WaiterTests {
+
+    @get:Rule
+    val mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.serhiivorobiov.smack", appContext.packageName)
+    fun waiterBringMenu() {
+       onView(withContentDescription("Open navigation drawer"))
+           .perform(click())
+        onView(withId(R.id.user_image_nav_header))
+            .check(matches(isDisplayed()))
+            onView(withId(R.id.login_btn_nav_header))
+                .perform(click())
     }
 }
-
-
